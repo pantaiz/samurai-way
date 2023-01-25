e2815eadaf472734ccb9f0fdbefe886858276479
Pantaiz_Lenovo <pantaiz@yandex.ru>
2023-01-25 12:30:38 +0300

    11 chapter

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,37 @@
 import React from 'react';
 import './App.css';
-import  Technologies from "./Technologies";
-import Header from "./Header";
 
 function App() {
-  return (
-    <div className="App">
-        <div><Header/>
-        <Technologies/>
+    return (
+        <div className={'app-wraper'}>
+            <header className={'header'}>
+                <img src='https://www.logolynx.com/images/logolynx/56/567ad0122e6b61fd940ab566f6fd6598.png'/>
+            </header>
+            <nav className={'nav'}>
+                <div><a href={'#'}>Profile</a></div>
+                <div><a href={'#'}>Messages</a></div>
+                <div><a href={'#'}>News</a></div>
+                <div><a href={'#'}>Settings</a></div>
+            </nav>
+            <div className={'content'}>
+                <div className={"background-Content"}>
+                    <img src={'https://img3.goodfon.ru/original/1280x1024/2/5a/pasture-the-bavarian-alps.jpg'}/>
+                </div>
+
+                <div>
+                    ava+description
+                </div>
+
+                <div>
+                    My post
+                    <div>New Post</div>
+                    <div>post 1</div>
+                    <div>post 2</div>
+                </div>
+            </div>
+
         </div>
-    </div>
-  );
+    );
 }
 
 
