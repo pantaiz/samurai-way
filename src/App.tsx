bb9982fa12fe346a0013d6126a37a2ed9bcff724
Pantaiz <pantaiz@yandex.ru>
2023-01-27 12:41:56 +0300

    18 lesson

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,18 @@ import './App.css';
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
+import Dialogs from "./components/Dialogs/Dialogs";
 
 function App() {
     return (
         <div className={'app-wraper'}>
             <Header/>
             <Navbar/>
-            <Profile/>
+            <div className={"app-wraper-content"}>
+             {/*<Profile/>*/}
+             <Dialogs></Dialogs>
+            </div>
+
         </div>
     );
 }
---
+++ src/components/Dialogs/Dialogs.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import s from './Dialogs.module.css';
+
+const Dialogs = () => {
+    return (<div>
+            DIALOGS
+
+        </div>
+    )
+
+}
+export default Dialogs
