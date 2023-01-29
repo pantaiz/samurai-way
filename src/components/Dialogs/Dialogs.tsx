94573f7dc84e64cfea7e6b9cf5b4588c459de01c
Pantaiz <pantaiz@yandex.ru>
2023-01-29 12:46:58 +0300

    22 lesson Сделал урок. Написал роуты для диалогов и лучшил верстку. Гриды имба. Иду писать конспект((((( Димыч сказал, что типо если нету конспекта то иди отсюда(((((((((((((((((( Грусть, обида, печаль, тоска. БЛЯЯЯЯ уже 21 день подряд минимум 5 коммитов.

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,20 +1,30 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import s from './Dialogs.module.css';
 
 const Dialogs = () => {
     return (<div className={s.dialogs}>
             <div className={s.dialogsItems}>
-                <div className={s.dialog+' '+s.active}>Dimych</div>
-                <div className={s.dialog}>Marharita</div>
-                <div className={s.dialog}>Roma</div>
-                <div className={s.dialog}>Vitalya</div>
-                <div className={s.dialog}>Artem</div>
-                <div className={s.dialog}>Jenya</div>
+                <div className={s.dialog+' '+s.active}>
+                    <NavLink to={"/dialogs/1"} activeClassName={s.activelink}>Dimych</NavLink> </div>
+                <div className={s.dialog}>
+                    <NavLink to={"/dialogs/2"} activeClassName={s.activelink}>Marharita</NavLink></div>
+                <div className={s.dialog}>
+                    <NavLink to={"/dialogs/3"} activeClassName={s.activelink}>Roma</NavLink></div>
+                <div className={s.dialog}>
+                    <NavLink to={"/dialogs/4"} activeClassName={s.activelink}>Vitalya</NavLink></div>
+                <div className={s.dialog}>
+                    <NavLink to={"/dialogs/5"} activeClassName={s.activelink}>Artem</NavLink></div>
+                <div className={s.dialog}>
+                    <NavLink to={"/dialogs/6"} activeClassName={s.activelink}>Jenya</NavLink></div>
             </div>
         <div className={s.messages}>
             <div className={s.message}>Hiasd</div>
+            <div className={s.message+' '+s.mymessage}>Darova chel</div>
             <div className={s.message}>It's test message</div>
+            <div className={s.message+' '+s.mymessage}>Ti cho blya?</div>
             <div className={s.message}>Vilkoy v glaz?</div>
+            <div className={s.message+' '+s.mymessage}>Aga</div>
         </div>
         </div>
     )
