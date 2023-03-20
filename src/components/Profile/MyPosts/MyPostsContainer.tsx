b5ed0cd266280ed70c06054d6ff6a6ee957355fb
Pantaiz <pantaiz@yandex.ru>
2023-03-20 23:26:49 +0300

    43.01

---
+++ src/components/Profile/MyPosts/MyPostsContainer.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import s from './MyPosts.module.css';
+import Post from "./Post/Post";
+import {dispatchType, MyPostsData} from "../../../index";
+import { AddPost } from "./AddPost/AddPost";
+
+type MyPostsProps = {
+    myPostsData: MyPostsData
+    dispatch:dispatchType
+}
+
+const MyPosts = (props: MyPostsProps) => {
+    return (
+        <div className={s.MyPosts}>
+                <AddPost dispatch={props.dispatch}
+                         AddPostData={props.myPostsData.AddPostData}/>
+                <Post postData={props.myPostsData.PostData}/>
+        </div>
+    )
+
+}
+
