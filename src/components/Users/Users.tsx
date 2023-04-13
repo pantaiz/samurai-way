768f7ee3a73e19a2f36acd0ddc32859b9e1bb68e
Pantaiz <pantaiz@yandex.ru>
2023-04-14 00:58:57 +0300

    53 completed

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -1,73 +1,44 @@
 import React from "react";
-import s from './Users.module.css';
-import {UsersPropsType} from "./UsersContainer";
-import {v1} from "uuid";
+import s from "./Users.module.css";
+import userPhoto from "../../assets/image/user.jpg";
 import axios from "axios";
-import userPhoto from '../../assets/image/user.jpg'
-export const Users = (props:UsersPropsType) => {
-let users=[
-        {
-            id: v1(),
-            photoURL:'https://www.meme-arsenal.com/memes/9021a567c31b9d2326dbe4ad0fafa5f4.jpg',
-            followed: true,
-            fullName: 'Dmitry',
-            status: 'Iam a boss',
-            location: {city: 'Minsk', country: 'Belarus'}
-        },
-        {
-            id: v1(),
-            photoURL:'https://www.meme-arsenal.com/memes/9021a567c31b9d2326dbe4ad0fafa5f4.jpg',
-            followed: false,
-            fullName: 'Sasha',
-            status: 'Iam a boss',
-            location: {city: 'Moskow', country: 'Russian'}
-        },
-        {
-            id: v1(),
-            photoURL:'https://www.meme-arsenal.com/memes/9021a567c31b9d2326dbe4ad0fafa5f4.jpg',
-            followed: true,
-            fullName: 'Andrew',
-            status: 'Iam a boss',
-            location: {city: 'Kiev', country: 'Ukraine'}
-        }, {
-        id: v1(),
-        photoURL:'https://www.meme-arsenal.com/memes/9021a567c31b9d2326dbe4ad0fafa5f4.jpg',
-        followed: true,
-        fullName: 'Andrew',
-        status: 'Iam a boss',
-        location: {city: 'Kiev', country: 'Ukraine'}
-    },
-    ]
 
-    if (props.users.length===0) {
+
+export class Users extends React.Component<any, any> {
+constructor(props:any) {
+    super(props);
+    if (this.props.users.length === 0) {
         axios.get('https://social-network.samuraijs.com/api/1.0/users ')
-            .then(response=>{
-                props.setUsers(response.data.items)
+            .then(response => {
+                this.props.setUsers(response.data.items)
             })
 
     }
+}
 
-    return (<div className={s.banner}>
-            {props.users.map(a=><div key={a.id}>
-                <span>
 
-                    <div >
-                        <img className={s.avatarimg} src={a.photos.small!=null? a.photos.small:userPhoto}/>
+    render() {
+        return (<div className={s.banner}>
+                {this.props.users.map((a:any) => <div key={a.id}>
+                    <span>
+                    <div>
+                        <img className={s.avatarimg} src={a.photos.small != null ? a.photos.small : userPhoto}/>
                     </div>
                     <div>
-                        {a.followed?
-                            <button onClick={()=>props.unfollow(a.id)}>UnFollow</button>:
-                            <button onClick={()=>props.follow(a.id)}>Follow</button>}
+                        {a.followed ?
+                            <button onClick={() => this.props.unfollow(a.id)}>UnFollow</button> :
+                            <button onClick={() => this.props.follow(a.id)}>Follow</button>}
 
                     </div>
                 </span>
-                <span>
-                    <span><div>{a.fullName}</div><div></div>{a.status}</span>
+                        <span>
+                    <span><div>{a.fullName}</div><div></div>
+                        {a.status}</span>
                     <span><div>{'a.location.country'}</div><div>{'a.location.city'}</div></span>
                 </span>
-            </div>)}
-            <p>USERS</p>
-        </div>
-    )
-
+                    </div>)}
+                <p>USERS</p>
+            </div>
+        )
+    }
 }
